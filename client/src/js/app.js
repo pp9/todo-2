eb19984b601ce192f809e550954fbf9c51b62f60
esimonenko <eugeny.simonenko@hiqo-solutions.com>
2016-04-20 18:45:52 +0300

    app update

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,11 +1,21 @@
-angular.module('todo', [])
+angular.module('todo', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'client/src/html/login.html',
+                controller: 'LoginCtrl'
+            })
+            .when('/index', {
+                templateUrl: 'client/src/html/index.html',
+                controller: 'MainCntr'
+            })
+    })
     .service('mainService', function($http) {
         var data = [];
 
         function getData() {
             return $http.get('/db/app.json')
                 .then(function(data) {
-                    // data = angular.fromJson(data.data);
                     return angular.fromJson(data.data);
                 })
         }
@@ -13,7 +23,47 @@ angular.module('todo', [])
             getData: getData
         }
     })
-    .controller('MainCntr', function($scope, $http, mainService) {
+    .controller('LoginCtrl', function($scope, $http, $location, $rootScope) {
+
+        $('#login-modal').modal('show');
+
+        $scope.submitPassword = function() {
+            var json = angular.toJson({
+                "password": $scope.password
+            });
+            var req = {
+             method: 'POST',
+             url: '/password',
+             headers: {
+               'Content-Type': 'application/json'
+             },
+             data: json
+            }
+
+            $http(req).then(function(data){
+                if(data.data === 'true') {
+                    $rootScope.login = true;
+                    $('#login-modal').modal('hide');
+                    $('body').removeClass('modal-open');
+
+                    $location.path( "/index" );
+                    // $('#login-modal').on('hidden.bs.modal ', function() {
+                    //     $location.path( "/index" );
+                    // });
+                }
+            }, function(){
+                console.log('error');
+            });
+            // console.log($scope.password);
+
+        }
+    })
+    .controller('MainCntr', function($rootScope, $scope, $http, mainService, $location) {
+        // $('.modal').modal('hide');
+        $('.modal-backdrop').remove();
+
+        if(!$rootScope.login) $location.path( "/" );
+
         $scope.tasks = [];
         $scope.currentTask;
 
@@ -23,20 +73,41 @@ angular.module('todo', [])
             this.text = item.text || '';
         }
 
+        const Task = function(task) {
+            this.id = task.id || $scope.tasks.length;
+            this.name = task.name || 'Name';
+            this.textNote = task.textNote || 'Text';
+            this.items = task.items || [];
+        }
+
         mainService.getData()
             .then(function(data) {
                 $scope.tasks = data.tasks;
+                // console.log(data);
                 // console.log($scope.tasks)
             })
+
+
         $scope.editTask = function(item) {
             $scope.currentTask = item;
             $('#modal-id').modal('show');
         }
+
+        $scope.addTask = function() {
+            let task = new Task({});
+            $scope.tasks.push(task);
+        }
+
+        $scope.removeTask = function(index) {
+            $scope.tasks.splice(index, 1);
+        }
+
         $scope.addItem = function() {
             let newItem = new Item({});
             $scope.currentTask.items.push(newItem);
             // console.log(newItem);
         }
+        
         $scope.removeItem = function(index) {
             $scope.currentTask.items.splice(index, 1);
         }
--- server/server.js
+++ server/server.js
@@ -5,28 +5,33 @@ const http = require('http');
 const url = require('url');
 const mime = require('mime');
 const path = require('path');
+const crypto = require('crypto');
 
 const clientPath = path.resolve(__dirname, '../client');
 const canonical = path.resolve(__dirname, '../');
 // console.log(canonical);
 const db = path.resolve(__dirname, '../db/app.json');
+const pass = path.resolve(__dirname, '../db/pass.txt');
 
 const server = http.createServer(function(req, res) {
     if(req.method === 'GET') {
+        // console.log('get');
         const rUrl = url.parse(req.url, true);
-        if(rUrl.query !== null) {
-            switch(rUrl.query.data) {
-                case 'all':
-                    fs.readFile(db, 'utf8', function(err, data) {
-                        if(err) return console.log(err);
-                        res.writeHeader(200);
-                        res.end(data);
-                    })
-                    break;
-            }
-        }
+        // console.log(rUrl);
+        // if(rUrl.query !== null) {
+        //     switch(rUrl.query.data) {
+        //         case 'all':
+        //             // console.log('all');
+        //             fs.readFile(db, 'utf8', function(err, data) {
+        //                 if(err) return console.log(err);
+        //                 res.writeHeader(200);
+        //                 res.end(data);
+        //             })
+        //             break;
+        //     }
+        // }
         let requistPatch = rUrl.path;
-        console.log(requistPatch);
+        // console.log(requistPatch);
         let indexPatch = path.resolve(canonical, 'index.html');
         switch (requistPatch) {
             case '/':
@@ -43,26 +48,49 @@ const server = http.createServer(function(req, res) {
             default:
                 let pth = canonical + requistPatch;
                 pth = path.resolve(pth);
-                // console.log(pth);
                 var mm = mime.lookup(pth);
                 fs.readFile(pth, 'utf8', function(err, data) {
                     if(err) return;
                     res.writeHeader(200, {'Content-Type': mm});
                     res.end(data);
                 });
-                break;
         }
     }else {
-        let rUrl = url.parse(req.url, true);
-        let body = '';
-        req.on('data', (d) => {
-            body += d;
-            // console.log(body);
-            fs.writeFile(db, body);
-        });
+        // POST
+        console.log('post');
 
-        res.writeHeader(200);
-        res.end();
+        let rUrl = url.parse(req.url, true);
+
+        switch(rUrl.path) {
+            case '/data':
+                let body = '';
+                req.on('data', (d) => {
+                    body += d;
+                    fs.writeFile(db, body);
+                });
+                break;
+            case '/password':
+                req.on('data', data => {
+                    var pass_shasum = crypto.createHash('sha256').update(data).digest('hex');
+                    // fs.writeFile(pass, pass_shasum);
+                    fs.readFile(pass, function(err, data) {
+                        let resp = 'false'
+                        if(data + '' === pass_shasum) {
+                            resp = 'true';
+                        }
+                        res.writeHeader(200);
+                        res.end(resp);
+                    })
+                })
+                break;
+            default:
+                res.writeHeader(200);
+                res.end();
+        }
+
+
+
+
         // if(rUrl.query !== null) {
         //     console.log();
         // }
